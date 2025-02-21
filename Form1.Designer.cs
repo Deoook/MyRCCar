08b7b954011a31b215b6a1b593667e6e16a65cfd
gusej <gusej@DESKTOP-U09MRMK>
2025-02-21 20:40:35 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace MyRCCar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbPort
+            // 
+            this.tbPort.Location = new System.Drawing.Point(6, 103);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(86, 25);
+            this.tbPort.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "IP";
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(6, 49);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(134, 25);
+            this.tbIP.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblResult);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.tbIP);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.tbPort);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(325, 166);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(174, 27);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(133, 47);
+            this.btnConnect.TabIndex = 4;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(174, 112);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(45, 15);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "label3";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(136, 196);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(77, 52);
+            this.btnGo.TabIndex = 5;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Location = new System.Drawing.Point(56, 254);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(75, 52);
+            this.btnLeft.TabIndex = 6;
+            this.btnLeft.Text = "Left";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Location = new System.Drawing.Point(217, 254);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(75, 52);
+            this.btnRight.TabIndex = 7;
+            this.btnRight.Text = "Right";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(136, 312);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(77, 52);
+            this.btnBack.TabIndex = 8;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(136, 254);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(77, 52);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Location = new System.Drawing.Point(359, 12);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.Size = new System.Drawing.Size(452, 398);
+            this.rtbLog.TabIndex = 10;
+            this.rtbLog.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 423);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbIP;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.RichTextBox rtbLog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyRCCar
+{
+    public partial class Form1 : Form
+    {
+        private TcpListener listener;
+        private TcpClient client;
+        private NetworkStream stream;
+        private Thread serverThread;
+        private Thread receiveThread;
+        private bool isRunning = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            tbIP.Text = "192.168.0.145";
+            lblResult.Text = "";
+
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (isRunning)
+            {
+                WriteLog("서버가 이미 실행 중입니다.");
+                return;
+            }
+
+            string ip = tbIP.Text;
+            string port = tbPort.Text;
+
+            if(!IPAddress.TryParse(ip, out IPAddress iPAddress) || !int.TryParse(port, out int portt))
+            {
+                WriteLog("올바른 IP와 포트 번호를 입력하세요."); 
+                return;
+            }
+
+            serverThread = new Thread(()=>StartServer(iPAddress, portt));
+            serverThread.IsBackground = true;
+            serverThread.Start();
+        }
+
+        private void StartServer(IPAddress ip, int port)
+        {
+            try
+            {
+                listener = new TcpListener(ip, port);
+                listener.Start();
+                isRunning = true;
+                WriteLog($"서버 시작 : {ip}:{port}");
+                WriteLog("클라이언트 연결 대기 중...");
+
+                client = listener.AcceptTcpClient();
+                stream = client.GetStream();
+                WriteLog("클라이언트가 연결되었습니다.");
+                lblResult.Text = "Waiting...";
+            }
+            catch (Exception ex)
+            {
+                WriteLog($"서버 오류 : {ex.Message}");
+                lblResult.Text = "Error";
+            }
+        }
+
+
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            SendMessage("GO");
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            SendMessage("STOP");
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            SendMessage("BACK");
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            SendMessage("LEFT");
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            SendMessage("RIGHT");
+        }
+
+        private void SendMessage(string message)
+        {
+            if (client == null || !client.Connected)
+            {
+                WriteLog("클라이언트가 연결되지 않았습니다");
+                return;
+            }
+
+            try
+            {
+                byte[] buffer = Encoding.UTF8.GetBytes(message);
+                stream.Write(buffer, 0, buffer.Length);
+                WriteLog($"Send [{message}]");
+            }
+            catch (Exception ex)
+            {
+                WriteLog($"전송 오류 : {ex.Message}");
+            }
+        }
+
+        private void WriteLog(string log)
+        {
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            rtbLog.AppendText($"{timestamp} : {log}\n");
+        }
+    }
+}
